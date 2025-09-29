5c4c7561ca6f541b4f033e69ec2375539b4b78c6
SamuelZambrana <samu-1994@hotmail.es>
2025-09-29 08:45:58 +0200

    ADD: APP vite explications diferents setup-script stup

---
+++ indecission-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Hola dev</h1>
+
+  <hr />
+
+  <MuyCounter :value="5"/>
+  <MyCounterScript :value="5" />
+  <MyCounterScript2 :value="5" />
+
+</template>
+
+<script lang="ts" setup>
+import MuyCounter from './components/MuyCounter.vue';
+import MyCounterScript from './components/MyCounterScript.vue';
+import MyCounterScript2 from './components/myCounterScript/MyCounterScript2.vue';
+
+
+</script>
+
+<style>
+
+</style>
---
+++ indecission-app/src/components/myCounterScript/MyCounterScript2.vue
@@ -0,0 +1,24 @@
+<template >
+  <section>
+
+    <h3>counter: {{counter}}</h3>
+    <h3>Square: {{squareCounter}}</h3>
+
+    <div>
+      <button @click="counter++">+1</button>
+      <button @click="counter--">-1</button>
+    </div>
+  </section>
+
+</template>
+
+<script lang="ts" src="./MyCounterScript2">
+
+
+</script>
+
+<style scoped>
+
+
+
+</style>
